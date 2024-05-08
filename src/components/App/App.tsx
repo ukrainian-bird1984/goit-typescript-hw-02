cb5ed7890b8390f67bf9ea5065b2566776a169de
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-08 22:33:23 +0200

    last2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { fetchPhotosByQuery, PhotoData } from '../api';
-
 import SearchBar from '../SearchBar/SearchBar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 import { ImageModal } from '../ImageModal/ImageModal';
-
 import css from './App.module.css';
 
 const App: React.FC = () => {
@@ -19,16 +17,15 @@ const App: React.FC = () => {
   const [loadMore, setLoadMore] = useState<boolean>(false);
   const [content, setContent] = useState<string | null>(null);
   const [query, setQuery] = useState<string>('');
-  const [modalIsOpen, setIsOpen] = useState<boolean>(false);
 
-  const userQuery = (value: string) => {
+  const userQuery = (value: string): void => {
     setQuery(value);
     setPage(1);
     setPhotos(null);
   };
 
   useEffect(() => {
-    async function fetchPhotos() {
+    async function fetchPhotos(): Promise<void> {
       if (query === '') return;
       try {
         setLoading(true);
@@ -62,7 +59,7 @@ const App: React.FC = () => {
     }
   }, [query, page]);
 
-  const loadMorePhotos = () => {
+  const loadMorePhotos = (): void => {
     if (page <= response.total_pages) {
       setPage(page + 1);
     } else {
@@ -71,39 +68,39 @@ const App: React.FC = () => {
     }
   };
 
-  const handleImageClick = (url: string) => {
+  const handleImageClick = (url: string): void => {
     setContent(url);
   };
 
-  const openModal = () => {
+  const openModal = (): void => {
     setIsOpen(true);
   };
 
-  const closeModal = () => {
+  const closeModal = (): void => {
     setIsOpen(false);
   };
 
   return (
     <div className={css.container}>
-      <SearchBar onSubmit={userQuery}></SearchBar>
+      <SearchBar onSubmit={userQuery} />
       {Array.isArray(photos) && (
         <ImageGallery
           collection={photos}
           onPhotoClick={handleImageClick}
           openModal={openModal}
-        ></ImageGallery>
+        />
       )}
-      {error && <ErrorMessage></ErrorMessage>}
-      {loading && <Loader></Loader>}
+      {error && <ErrorMessage />}
+      {loading && <Loader />}
       {loadMore && (
-        <LoadMoreBtn loadMoreScroll={photos} onLoadMoreBtn={loadMorePhotos}></LoadMoreBtn>
+        <LoadMoreBtn loadMoreScroll={photos} onLoadMoreBtn={loadMorePhotos} />
       )}
       <ImageModal
         onOpenButton={openModal}
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         content={content}
-      ></ImageModal>
+      />
     </div>
   );
 };
