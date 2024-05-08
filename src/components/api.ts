2a1bb6fdbefbd288d6512eff0bc6b58e12abbafe
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-08 23:39:27 +0200

    исмт

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-export const fetchPhotosByQuery = async (query: string, page: number): Promise<any> => {
+export const fetchPhotosByQuery = async (query: string, page: number): Promise<PhotoData> => {
   const Access_Key: string = 'kcbG2Jgq2oXSu1iIa5Mf1PUXvAgyf4nTnbStAzDMij0';
   const baseApiUrl: string = 'https://api.unsplash.com/search/photos';
   const apiQuery: string = `${baseApiUrl}?page=${page}&query=${query}&client_id=${Access_Key}`;
