b3ca5e095fc775ec52f24159591d2c15cca7dac2
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-09 19:59:13 +0200

    ипартапт

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import ImageCard from '../ImageCard/ImageCard';
-import css from './ImageGallery.module.css';
-import { Photo } from '../../types';
+import React, { useEffect, useState } from 'react';
+import { fetchPhotosByQuery } from '../api';
+import SearchBar from '../SearchBar/SearchBar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import ImageModal from '../ImageModal/ImageModal';
+import css from './App.module.css';
 
-interface ImageGalleryProps {
-  collection: Photo[];
-  onPhotoClick: (url: string) => void;
-  openModal: () => void;
+interface Photo {
+  id: string;
+  urls: {
+    regular: string;
+    thumb: string;
+  };
 }
 
-const ImageGallery: React.FC<ImageGalleryProps> = ({ collection, onPhotoClick, openModal }) => {
+const App: React.FC = () => {
+  const [photos, setPhotos] = useState<Photo[] | null>(null);
+  const [query, setQuery] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | undefined>(undefined);
+  const [scrollBtn, setScrollBtn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth > 900) {
+        const isScrollBtnVisible = window.scrollY > 200;
+        setScrollBtn(isScrollBtnVisible);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!query) return;
+    async function handleSearch() {
+      try {
+        setLoading(true);
+        setError(false);
+        const data = await fetchPhotosByQuery(query, page);
+        setPhotos(prevPhotos => {
+          if (Array.isArray(prevPhotos)) {
+            return [...prevPhotos, ...data.results];
+          } else {
+            return [...data.results];
+          }
+        });
+        setTotalPages(data.total_pages || 0);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    handleSearch();
+  }, [query, page]);
+
+  const handleQuery = (newQuery: string) => {
+    if (newQuery !== query) {
+      setQuery(newQuery);
+      setPhotos(null);
+      setPage(1);
+      setTotalPages(0);
+    }
+  };
+
+  const loadMorePhotos = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const openModal = (photo: Photo) => {
+    setSelectedPhoto(photo);
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(undefined);
+  };
+
+  const onScrollBtn = () => {
+    setScrollBtn(false);
+  };
+
   return (
-    <ul className={css.image}>
-      {collection.map(item => (
-        <li key={item.id}>
-          <ImageCard image={item} onPhotoClick={onPhotoClick} openModal={openModal} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <SearchBar onSubmit={handleQuery} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {photos !== null && (
+        <ImageGallery collection={photos} onPhotoClick={openModal} openModal={openModal} />
+      )}
+      {totalPages > page && <LoadMoreBtn onLoadMoreBtn={loadMorePhotos} />}
+      <ImageModal isOpen={!!selectedPhoto} onRequestClose={closeModal} />
+      {scrollBtn && <ScrollIntoView selector="#header"><ScrollUp onScrollBtn={onScrollBtn} /></ScrollIntoView>}
+    </div>
   );
 };
 
-export default ImageGallery;
+export default App;
